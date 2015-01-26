07147340eb64a5572da5f3a797131b5c2de118cc
Michael Maliniak <michaelmaliniak@michaels-mbp.home>
2015-01-26 22:22:48 +1100

    added concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
 
-gulp.task('log', function(){
-	gutil.log("Workflow initiated");
+	var jsSources = [
+	'components/scripts/*'];
+
+gulp.task('js', function
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
